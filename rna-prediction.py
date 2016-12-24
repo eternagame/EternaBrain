565f78d0e0f8e83ac7693f0a186e4fa32b62ac12
RK900 <rovik05@gmail.com>
2016-12-24 13:30:40 -0800

    Parsed movesets using dataframe rather than list

--- rna-prediction.py
+++ rna-prediction.py
@@ -1,10 +1,7 @@
 import os
-
 file = os.getcwd() + '\movesets\epicfalcon.txt'
 
-with open(file) as epicfalcon:
-    epicfalcon = epicfalcon.readlines()
-
-print (epicfalcon)
-
+import pandas as pd
+epicfalcon = pd.read_csv(file, sep=" ", header='infer', delimiter='\t')
+print epicfalcon
 
