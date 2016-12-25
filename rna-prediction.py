d07cd1ab92130259eb3dbdf18c058c5997662d97
RK900 <rovik05@gmail.com>
2016-12-24 20:17:47 -0800

    Convert dataframe to dictionary and access specific moves

--- rna-prediction.py
+++ rna-prediction.py
@@ -3,6 +3,20 @@ file = os.getcwd() + '\movesets\epicfalcon.txt'
 
 import pandas as pd
 epicfalcon = pd.read_csv(file, sep=" ", header='infer', delimiter='\t')
-#print epicfalcon
+# epicfalcon is a dataframe containging epicfalcon.txt data
 
-print epicfalcon[['move_set']].ix[[0]]
+ms1 = epicfalcon[['move_set']].ix[[0]]
+
+ms2 = ms1.to_dict()
+
+ms3 = (ms2['move_set'])
+
+ms4 = ms3[0]
+
+#print(ms4)
+
+import ast
+
+ms5 = ast.literal_eval(ms4)
+
+print (ms5['moves'][0])
