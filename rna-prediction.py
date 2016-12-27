4473c2f7d2da3fffcf7ae47848b959d1cd49d10d
RK900 <rovik05@gmail.com>
2016-12-26 20:21:11 -0800

    Add descriptions of converting str file to dicts of movesets

--- rna-prediction.py
+++ rna-prediction.py
@@ -6,14 +6,15 @@ epicfalcon = pd.read_csv(file, sep=" ", header='infer', delimiter='\t')
 # epicfalcon is a dataframe containging epicfalcon.txt data
 
 ms1 = epicfalcon[['move_set']].ix[[0]]
-
+# ms1 is moveset for pid 6502951
 ms2 = ms1.to_dict()
-
+# ms2 is dictionary of ms1
 ms3 = (ms2['move_set'])
-
+print ms3
+# ms3 is moveset of ms2
 ms4 = ms3[0]
-
-#print(ms4)
+# ms4 is 
+print ms4
 
 import ast
 
