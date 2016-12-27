d9b7d5c03e26b013bfa388bf70348fe2bdd49a29
RK900 <rovik05@gmail.com>
2016-12-27 12:29:25 -0800

    Add comments and cleaned up code

--- rna-prediction.py
+++ rna-prediction.py
@@ -1,10 +1,12 @@
 import os
-file = os.getcwd() + '\movesets\epicfalcon.txt'
-
 import pandas as pd
+import ast
+
+file = os.getcwd() + '\movesets\epicfalcon.txt'
 epicfalcon = pd.read_csv(file, sep=" ", header='infer', delimiter='\t')
 # epicfalcon is a dataframe containging epicfalcon.txt data
 
+''' example of converting moveset data to dictionary
 ms1 = epicfalcon[['move_set']].ix[[1]]
 # ms1 is moveset for pid 6502951
 ms2 = ms1.to_dict()
@@ -14,19 +16,19 @@ ms3 = (ms2['move_set'])
 ms4 = ms3[1]
 # ms4 is ms3 w/o labels
 
-import ast
 
 ms5 = ast.literal_eval(ms4)
 #print (ms5['moves'])
 # ms5 is dict of ms4
 # can now be indexed like a normal dictionary
+'''
 
 movesets = [] # a list of dictionaries containing the movesets
-for i in range(101):
-    step1 = epicfalcon[['move_set']].ix[[i]]
-    step2 = step1.to_dict()
-    step3 = step2['move_set']
-    step4 = step3[i]
-    step5 = ast.literal_eval(step4)
-    movesets.append(step5['moves'])
+for i in range(101): # 102 total moveset solutions in epicfalcon.txt
+    step1 = epicfalcon[['move_set']].ix[[i]] # str of pid, sol_id, uid, and moveset
+    step2 = step1.to_dict() # dictionary of data
+    step3 = step2['move_set'] # selecting only moveset data
+    step4 = step3[i] # getting rid of labels
+    step5 = ast.literal_eval(step4) # converting movesets to dictionary
+    movesets.append(step5['moves']) # adding each moveset to list
     
