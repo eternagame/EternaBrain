8f784cb014b40c28bab5fd4128ae4c9762174db7
RK900 <rovik05@gmail.com>
2016-12-28 11:29:22 -0800

    Read in puzzle-structure-data.txt

--- rna-prediction.py
+++ rna-prediction.py
@@ -1,6 +1,9 @@
 import os
 from movesetreader import read_movesets
+from structure import read_structure
 
+# read moveset file
+# 102 total movesets
 epicfalcon = os.getcwd() + '\movesets\epicfalcon.txt'
 
 movesets = read_movesets(epicfalcon)
@@ -8,4 +11,9 @@ movesets = read_movesets(epicfalcon)
 print(movesets[0][0][0]['pos']) # first puzzle, first move, position of move
 print(movesets[15][24][0]['base']) # 16th puzzle, 25th move, base(AUGC) of move
 
+# read puzzle structure data file
+puzzle_structure_data = os.getcwd() + '\movesets\puzzle-structure-data.txt'
 
+structure = read_structure(puzzle_structure_data)
+
+print(structure['structure'][20672]) # 20,672 solutions
