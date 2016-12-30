1236c06588fe49d785bdfb355fd0bde2b5f7f877
RK900 <rovik05@gmail.com>
2016-12-30 12:15:48 -0800

    Call get_puzzleID to obtain pid

--- rna-prediction.py
+++ rna-prediction.py
@@ -1,5 +1,5 @@
 import os
-from movesetreader import read_movesets
+from movesetreader import read_movesets, get_puzzleID
 from structure import read_structure
 
 # read moveset file
@@ -7,6 +7,8 @@ from structure import read_structure
 epicfalcon = os.getcwd() + '\movesets\epicfalcon.txt'
 
 movesets = read_movesets(epicfalcon)
+pid = get_puzzleID(epicfalcon)
+print pid[100]
 
 #print(movesets[0][0][0]['pos']) # first puzzle, first move, position of move
 #print(movesets[15][24][0]['base']) # 16th puzzle, 25th move, base(AUGC) of move
