042feaa5757ee0693f0b9cd3a9b7e59a3a925011
RK900 <rovik05@gmail.com>
2017-01-05 18:14:22 -0800

    Find index of pid in puzzle info dataset and get structure

--- rna-prediction.py
+++ rna-prediction.py
@@ -1,6 +1,7 @@
 import os
 from movesetreader import read_movesets, get_puzzleData
 from structure import read_structure
+import pandas as pd
 
 # read moveset file
 # 102 total movesets
@@ -22,7 +23,18 @@ print sol_id[100]
 print uid[100] # uid will be the same as they are only 1 player's solutions
 print movesets[100]
 
+print type(structure['pid'])
+pid = structure['pid']
 
+print pid[pid==6503049]
+print structure['structure'][19195]
+
+'''
+pid2 = pd.Series(pid)
+print type(pid2)
+
+print pid2[pid2==6503049]
+'''
 #print(movesets[0][0][0]['pos']) # first puzzle, first move, position of move
 #print(movesets[15][24][0]['base']) # 16th puzzle, 25th move, base(AUGC) of move
 #print(structure['structure'][20672]) # 20,673 solutions
