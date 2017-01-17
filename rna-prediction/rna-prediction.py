619a7174968fd0ad31d836b7236693e601f160e7
RK900 <rovik05@gmail.com>
2017-01-16 16:02:14 -0800

    Encode sample RNA structure

--- rna-prediction/rna-prediction.py
+++ rna-prediction/rna-prediction.py
@@ -2,6 +2,7 @@ import os
 from movesetreader import read_movesets, puzzle_attributes
 from structure import read_structure
 from getData import getData_pid
+from encode_RNA import encode_structure
 
 # read moveset file
 # 102 total movesets
@@ -30,23 +31,9 @@ ms_6503049,stctr_6503049 = getData_pid(6503049,pid,movesets,structure)
 ms_6502960,stctr_6502960 = getData_pid(6502960,pid,movesets,structure)
 #print ms_6502960,stctr_6502960
 
-print type(stctr_6502960)
+enc_stctr_6502960 = encode_structure(stctr_6502960)
+print enc_stctr_6502960
 
-encoded_structure = []
-for i in stctr_6502960:
-  if i == ".":
-    encoded_structure.append([0])
-  elif i == "(" or i == ")":
-    encoded_structure.append([1])
-    
-  #print encoded_structure
-    
-  
-print encoded_structure
-
-
-def abc(s):
-  pass
 '''
 print pid.index(6503049)
 
