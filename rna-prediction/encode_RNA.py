f61a941724f5bad4197f7fb96a79ab91b33bcb68
RK900 <rovik05@gmail.com>
2017-01-23 17:57:02 -0800

    Make function to encode moveset

--- rna-prediction/encode_RNA.py
+++ rna-prediction/encode_RNA.py
@@ -20,6 +20,31 @@ def encode_structure(structure):
   return encoded_structure
 
 def encode_movesets(moveset):
-  pass
+  encoded_ms = []
+  for move in moveset:
+    if move[0]['base'] == 'A':
+      encoded_ms.append([1,move[0]['pos']])
+    elif move[0]['base'] == 'U':
+      encoded_ms.append([2,move[0]['pos']])
+    elif move[0]['base'] == 'G':
+      encoded_ms.append([3,move[0]['pos']])
+    elif move[0]['base'] == 'C':
+      encoded_ms.append([4,move[0]['pos']])
+    
+  return encoded_ms
 
+'''
+encoded_ms = []
+for i in ms_6503049:
+  print (i[0]['pos'])
+  if i[0]['base'] == 'A':
+    encoded_ms.append([1,i[0]['pos']])
+  elif i[0]['base'] == 'U':
+    encoded_ms.append([2,i[0]['pos']])
+  elif i[0]['base'] == 'G':
+    encoded_ms.append([3,i[0]['pos']])
+  elif i[0]['base'] == 'C':
+    encoded_ms.append([4,i[0]['pos']])
 
+print encoded_ms
+'''
--- rna-prediction/rna-prediction.py
+++ rna-prediction/rna-prediction.py
@@ -24,17 +24,6 @@ structure = read_structure(puzzle_structure_data)
 ms_6503049,stctr_6503049 = getData_pid(6503049,pid,movesets,structure)
 ms_6502960,stctr_6502960 = getData_pid(6502960,pid,movesets,structure)
 
-encoded_ms = []
-for i in ms_6503049:
-  print (i[0]['pos'])
-  if i[0]['base'] == 'A':
-    encoded_ms.append([1,i[0]['pos']])
-  elif i[0]['base'] == 'U':
-    encoded_ms.append([1,i[0]['pos']])
-  elif i[0]['base'] == 'G':
-    encoded_ms.append([1,i[0]['pos']])
-  elif i[0]['base'] == 'C':
-    encoded_ms.append([1,i[0]['pos']])
 
 
 '''
