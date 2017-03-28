5c8209aa5841e67c6b82ddb76235ddbbea172d4d
RK900 <rovik05@gmail.com>
2017-03-27 18:25:12 -0700

    Use pickled GMMs to plot clustering

---
+++ rna-prediction/graphs.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 27 18:09:56 2017
+
+@author: rohankoodli
+"""
+
+from sklearn.mixture import GaussianMixture
+import os
+from matplotlib import pyplot as plt
+import seaborn; seaborn.set()
+import pickle
+
+puzzle = raw_input("Puzzle ID: ")
+
+y_gmm = pickle.load(open(os.getcwd() + '/pickles/gmm-' + puzzle + '.p','rb'))
+transf = pickle.load(open(os.getcwd() + '/pickles/pca-' + puzzle + '.p','rb'))
+
+plt.scatter(transf[:,0], transf[:,1],c=y_gmm, cmap='RdYlBu',s=150)
+plt.suptitle("Puzzle " + puzzle,fontsize=18)
