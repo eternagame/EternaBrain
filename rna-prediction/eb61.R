47446cee8785173038cfbec555a4c72f010aa3d7
Rohan Koodli <rovik05@gmail.com>
2017-09-03 22:19:09 -0700

    Generate graphs for Eterna100

---
+++ rna-prediction/eb61.R
@@ -0,0 +1,24 @@
+# install.packages("reshape2")
+library("ggplot2")
+library(reshape2)
+
+#data = read.table("/Users/rohankoodli/Desktop/results_5timeout.txt")
+#new.df = melt(data, id.vars='name', variable.name='name', value.name='solved')
+#ggplot(data) + geom_tile(aes(x=algorithm, y=name, fill=solved)) + scale_x_discrete("", labels=names) + scale_y_discrete("") + scale_fill_manual(values = c("#DD3333", "#33DD33"), na.value="gray50")+ theme(axis.text.x = element_text(angle = 90, colour=cols), legend.position="none")
+
+df = read.table('/combined_6timeout.txt', header=T)
+new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
+ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
+  scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + 
+  theme(axis.text.x = element_text(angle = 90), legend.position="none")
+
+#dev.off()
+
+#df2 = read.table('/Users/rohankoodli/Desktop/results_7timeout.txt', header=T)
+#new2.df = melt(df2, id.vars='name', variable.name='algorithm', value.name='solved')
+#ggplot(new2.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
+#  scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + #  theme(axis.text.x = element_text(angle = 90), legend.position="none")
+
+ggsave('/eb100-2.png')
+
+
