1ce2387bc6cf4d7d00883da0d01a6f576d2188ea
Rohan Koodli <rovik05@gmail.com>
2018-03-18 18:40:36 -0700

    Update R graphs

--- rna-prediction/eb61.R
+++ rna-prediction/eb61.R
@@ -6,9 +6,8 @@ library(reshape2)
 #new.df = melt(data, id.vars='name', variable.name='name', value.name='solved')
 #ggplot(data) + geom_tile(aes(x=algorithm, y=name, fill=solved)) + scale_x_discrete("", labels=names) + scale_y_discrete("") + scale_fill_manual(values = c("#DD3333", "#33DD33"), na.value="gray50")+ theme(axis.text.x = element_text(angle = 90, colour=cols), legend.position="none")
 
-png("/eb100-2.png",height=495,width = 315)
-
-df = read.table('~/Desktop/combined_6timeout.txt', header=T)
+png("/hyper_50-100",height=495,width = 315)
+df = read.table('~/Documents/EternaBrain-data-archive/sapresults1.txt', header=T)
 new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
 ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
   scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + 
@@ -21,6 +20,6 @@ ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale
 #ggplot(new2.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
 #  scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + #  theme(axis.text.x = element_text(angle = 90), legend.position="none")
 
-ggsave('~/Desktop/eb_1-50.png')
+ggsave('~/Desktop/hyper_1-50.png')
 
 
--- rna-prediction/eb612.R
+++ rna-prediction/eb612.R
@@ -1,12 +1,12 @@
 library(ggplot2)
 library(reshape2)
 
-png("/eb100-1.png",height=495,width = 330)
+png("/hyper_50-100.png",height=495,width = 330)
 
-df = read.table('~/Desktop/combined_7timeout.txt', header=T)
+df = read.table('~/Documents/EternaBrain-data-archive/sapresults2.txt', header=T)
 new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
 ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
   scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + 
   theme(axis.text.x = element_text(angle = 90), legend.position="none")
   
-ggsave('~/Desktop/eb_50-100.png')
+ggsave('~/Desktop/hyper_50-100.png')
