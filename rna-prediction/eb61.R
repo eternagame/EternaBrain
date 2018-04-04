70b6ad55b37d5d715b670a8e7a92985b53eabd77
Rohan Koodli <rovik05@gmail.com>
2018-04-04 14:17:17 -0700

    Update R graphs

--- rna-prediction/eb61.R
+++ rna-prediction/eb61.R
@@ -6,20 +6,27 @@ library(reshape2)
 #new.df = melt(data, id.vars='name', variable.name='name', value.name='solved')
 #ggplot(data) + geom_tile(aes(x=algorithm, y=name, fill=solved)) + scale_x_discrete("", labels=names) + scale_y_discrete("") + scale_fill_manual(values = c("#DD3333", "#33DD33"), na.value="gray50")+ theme(axis.text.x = element_text(angle = 90, colour=cols), legend.position="none")
 
-png("/hyper_50-100",height=495,width = 315)
-df = read.table('~/Documents/EternaBrain-data-archive/sapresults1.txt', header=T)
+png("eb-2222",height=495,width = 315)
+df = read.table('~/Documents/EternaBrain-data-archive/combined_7timeout.txt', header=T)
+df$EteRNABot...27 = NULL
+print(colnames(df))
+df <- df[c("name", "RNASSD...27", "RNAinverse...28", "DSSOPT...47", "NUPACK...48", "INFORNA...50", "MODENA...54", "EternaBrain...61")]
 new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
-ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
-  scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + 
+ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") +
+  scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") +
   theme(axis.text.x = element_text(angle = 90), legend.position="none")
 
 #dev.off()
 
-#df2 = read.table('/Users/rohankoodli/Desktop/results_7timeout.txt', header=T)
-#new2.df = melt(df2, id.vars='name', variable.name='algorithm', value.name='solved')
-#ggplot(new2.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
-#  scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + #  theme(axis.text.x = element_text(angle = 90), legend.position="none")
+# df2 = read.table('~/Documents/EternaBrain-data-archive/results_7timeout.txt', header=T)
+# print(colnames(df2))
+# df2 <- df2[c("name", "RNA.SSD...27", "RNAinverse...28", "DSS.Opt...47", "NUPACK...48", "INFO.RNA...50", "MODENA...54", "EternaBrain..61")]
+# 
+# new2.df = melt(df2, id.vars='name', variable.name='algorithm', value.name='solved')
+# ggplot(new2.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") +
+#   scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") +
+#   theme(axis.text.x = element_text(angle = 90), legend.position="none")
 
-ggsave('~/Desktop/hyper_1-50.png')
+ggsave('~/Desktop/eb-2222.png')
 
 
--- rna-prediction/eb612.R
+++ rna-prediction/eb612.R
@@ -4,6 +4,7 @@ library(reshape2)
 png("/hyper_50-100.png",height=495,width = 330)
 
 df = read.table('~/Documents/EternaBrain-data-archive/sapresults2.txt', header=T)
+df <- df[c("name", "CNN_alone...20", "SAP_alone...50", "No_energy...51", "No_sequence...51", "No_pairmap...52", "No_dropout...57", "No_dot_bracket...58", "EternaBrain...61")]
 new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
 ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
   scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + 
