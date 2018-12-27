f60ccb5bb3c3210c20d049954b6f334f1c899830
Rohan Koodli <rovik05@gmail.com>
2018-12-27 13:45:13 -0800

    Create figure to generate 92 svgs

---
+++ draw_rna/draw92.py
@@ -0,0 +1,27 @@
+# IN PYTHON2
+
+from subprocess import call
+import os
+import pandas as pd
+
+path_to_movesets = os.getcwd()[:47] + '/rna-prediction/movesets/'
+
+with open(path_to_movesets + 'teaching-puzzle-ids.txt') as f:
+    content = f.readlines()
+
+df = pd.read_csv(path_to_movesets + 'puzzle-structure-data.txt', sep=" ", header="infer", delimiter='\t')
+df = df.set_index('pid')
+
+
+
+teachingpuzzles = map(int, content)
+
+for pid in teachingpuzzles:
+    file = open(os.getcwd() + 's4/p%i.txt' % pid, 'wb')
+    file.write(str(pid))
+    struc = df.loc[pid, 'structure']
+    file.write('\n' + 'A'*len*struc)
+    file.write('\n' + struc)
+    file.close()
+
+    call(['python', 'draw_all.py', 's4/p%i.txt' % pid])
