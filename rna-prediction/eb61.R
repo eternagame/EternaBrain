bf326089928681b39cd2eb93fbe883c78fa32b27
Rohan Koodli <rovik05@gmail.com>
2019-05-30 11:18:44 -0700

    Resize fig 3

--- rna-prediction/eb61.R
+++ rna-prediction/eb61.R
@@ -1,26 +1,28 @@
 # install.packages("reshape2")
 # install.packages("ggplot2")
+#install.packages("svglite")
 library(ggplot2)
 library(reshape2)
+
 
 #data = read.table("/Users/rohankoodli/Desktop/results_5timeout.txt")
 #new.df = melt(data, id.vars='name', variable.name='name', value.name='solved')
 #ggplot(data) + geom_tile(aes(x=algorithm, y=name, fill=solved)) + scale_x_discrete("", labels=names) + scale_y_discrete("") + scale_fill_manual(values = c("#DD3333", "#33DD33"), na.value="gray50")+ theme(axis.text.x = element_text(angle = 90, colour=cols), legend.position="none")
 
-png("eb-2222",height=495,width = 335)
-df = read.table('~/cnnresults2.txt', header=TRUE, sep='\t')
+png("eb-2222",height=775,width = 450)
+df = read.table('/Users/rohankoodli/Desktop/cnnresults1.txt', header=TRUE, sep='\t')
 df$EteRNABot...27 = NULL
 print(colnames(df))
 
 df$name = as.character(df$name)
 df$name <- factor(df$name, levels=unique(df$name))
 
-df <- df[c("name", "RNASSD...27", "RNAinverse...28", "DSSOPT...47", "NUPACK...48", "INFORNA...50", "MODENA...54", "EternaBrain...61")]
+df <- df[c("name", "RNASSD...27", "RNAinverse...28", "DSSOPT...47", "NUPACK...48", "INFORNA...50", "MODENA...54", "EternaBrain_SAP...61")]
 new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
 
-ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") +
+image = ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") +
   scale_y_discrete(limits = rev(levels(df$name))) + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") +
-  theme(axis.text.x = element_text(angle = 90), legend.position="none")
+  theme(axis.text.x = element_text(angle = 90), legend.position="none", text=element_text(size=16))
 
 #dev.off()
 
@@ -33,6 +35,7 @@ ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale
 #   scale_y_discrete("") + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") +
 #   theme(axis.text.x = element_text(angle = 90), legend.position="none")
 
-ggsave('~/Desktop/eterna100_51-100_ordered.png')
+# ggsave(file='~/Desktop/eterna100_1-50_ordered.png', plot=image)
+ggsave(file='~/Desktop/LARGE_eterna100_1_50.svg')
 
 
--- rna-prediction/eb612.R
+++ rna-prediction/eb612.R
@@ -1,18 +1,18 @@
 library(ggplot2)
 library(reshape2)
 
-png("/hyper_50-100.png",height=495,width = 335)
+png("/hyper_50-100.png",height=775,width = 465)
 
-df = read.table('~/hyperparameterresults2.txt', header=T)
+df = read.table('~/Desktop/hyperparameterresults1.txt', header=T)
 
 df$name = as.character(df$name)
 df$name <- factor(df$name, levels=unique(df$name))
 
-df <- df[c("name", "CNN_alone...20", "SAP_alone...50", "No_energy...51", "No_sequence...51", "No_pairmap...52", "No_dropout...57", "No_dot_bracket...58", "EternaBrain...61")]
+df <- df[c("name", "CNN_alone...20", "SAP_alone...50", "No_energy...51", "No_sequence...51", "No_pairmap...52", "No_dropout...57", "No_dot_bracket...58", "EternaBrain_SAP...61")]
 new.df = melt(df, id.vars='name', variable.name='algorithm', value.name='solved')
 
 ggplot(new.df) + geom_tile(aes(x=algorithm, y=name, fill=solved == "0")) + scale_x_discrete("") + 
   scale_y_discrete(limits = rev(levels(df$name))) + scale_fill_manual(values = c("#33DD33", "#DD3333"), na.value="gray50") + 
-  theme(axis.text.x = element_text(angle = 90), legend.position="none")
+  theme(axis.text.x = element_text(angle = 90), legend.position="none", text=element_text(size=16))
   
-ggsave('~/Desktop/hyper100_51-100.png')
+ggsave('~/Desktop/LARGE_hyper100_1_50.svg')
